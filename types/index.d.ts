a0e81746b2e0ae793f55a92daba2b0f184e9d9fc
Karthik <karthik.bharadwaj@pickcel.com>
2024-03-08 18:06:15 +0530

    fix: updated types for the new feature

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,28 +1,32 @@
-declare module 'reevsocket' {
-    export interface Options {
-      maxAttempts?: number;
-      timeout?: number;
-      onConnect?: (event: Event) => void;
-      onClose?: (event: CloseEvent) => void;
-      onMessage?: (event: MessageEvent) => void;
-      onError?: (event: Event | ErrorEvent) => void;
-      onReconnecting?: (event: Event | ErrorEvent) => void;
-      onOverflow?: (event: Event | ErrorEvent) => void;
-      protocols?: string[];
-    }
-  
-    export interface Controller {
-      on: (event: string, listener: (...args: any[]) => void) => void;
-      event: (event: string, ...args: any[]) => void;
-      emit: (action: string, json: any) => void;
-      start: () => void;
-      retry: (e: Event | CloseEvent | ErrorEvent) => void;
-      json: (value: any) => void;
-      send: (value: string | ArrayBufferLike | Blob | ArrayBufferView) => void;
-      close: (code?: number, reason?: string) => void;
-    }
-  
-    function connect(url: string, options?: Options): Controller;
-    export = connect;
+declare module "reevsocket" {
+  export interface Options {
+    maxAttempts?: number;
+    delay?: number;
+    exponentialFactor?: number;
+    maxDelay?: number;
+    heartbeatInterval?: number;
+    disableHeartbeat?: boolean;
+    pongTimeoutInterval?: number;
+    onConnect?: (event: Event) => void;
+    onClose?: (event: CloseEvent) => void;
+    onMessage?: (event: MessageEvent) => void;
+    onError?: (event: Event | ErrorEvent) => void;
+    onReconnecting?: (event: Event | ErrorEvent) => void;
+    onOverflow?: (event: Event | ErrorEvent) => void;
+    protocols?: string[];
   }
-  
+
+  export interface Controller {
+    on: (event: string, listener: (...args: any[]) => void) => void;
+    event: (event: string, ...args: any[]) => void;
+    emit: (action: string, json: any) => void;
+    start: () => void;
+    retry: (e: Event | CloseEvent | ErrorEvent) => void;
+    json: (value: any) => void;
+    send: (value: string | ArrayBufferLike | Blob | ArrayBufferView) => void;
+    close: (code?: number, reason?: string) => void;
+  }
+
+  function connect(url: string, options?: Options): Controller;
+  export = connect;
+}
